75d2c2d9f1fcecb2a7ab21393c56d502f8ae4b1a
Slobodan Vujovic <slobodan.vujovic@zuehlke.com>
2022-05-03 06:52:09 +0200

    Display owners and vets

--- pet-clinic-web/src/main/java/com/vujo/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/vujo/sfgpetclinic/controllers/VetsController.java
@@ -1,13 +1,28 @@
 package com.vujo.sfgpetclinic.controllers;
 
+import com.vujo.sfgpetclinic.model.Vet;
+import com.vujo.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/vets")
 @Controller
 public class VetsController {
+
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
+    public String index(Model model) {
+        Set<Vet> vets = vetService.findAll();
+        model.addAttribute("vets", vets);
+
         return "vets/index";
     }
 }
