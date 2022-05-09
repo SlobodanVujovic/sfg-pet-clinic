57e96fa7a8d5394963c8b340ca28fc1776173a8c
Slobodan Vujovic <slobodan.vujovic@zuehlke.com>
2022-05-09 05:45:19 +0200

    Add profile to map services

--- pet-clinic-data/src/main/java/com/vujo/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/vujo/sfgpetclinic/services/map/VetMapService.java
@@ -4,11 +4,13 @@ import com.vujo.sfgpetclinic.model.Speciality;
 import com.vujo.sfgpetclinic.model.Vet;
 import com.vujo.sfgpetclinic.services.SpecialityService;
 import com.vujo.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
     private final SpecialityService specialityService;
 
